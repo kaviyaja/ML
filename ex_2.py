1203675cd5cd51900b1970bd6b43993ca0fe5271
kaviyaja <jayakumarkaviya22@gmail.com>
2025-08-05 12:03:39 +0530

    Update multiple_linear.py

--- ex_2.py
+++ ex_2.py
@@ -2,7 +2,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 df=pd.read_csv("house.csv")
